08bf9813e35da8fef7b8ce097739493c71d77425
Sarah Rainsberger <sarah@rainsberger.ca>
2022-10-30 15:52:55 +0000

    install image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-
 import vue from "@astrojs/vue";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rainsberger.ca',
-  integrations: [react(), svelte(), vue()],
+  integrations: [react(), svelte(), vue(), 
+    
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })
+  
+  ],
   legacy: {
     // Example: Add support for legacy Markdown features
-    astroFlavoredMarkdown: true,
-  },
+    astroFlavoredMarkdown: true
+  }
 });
