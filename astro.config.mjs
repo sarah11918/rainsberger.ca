a3b80e4080639848947ad533266fa8ab959045ef
Sarah Rainsberger <sarah@rainsberger.ca>
2023-06-01 19:07:44 +0000

    experimental image assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,10 @@ import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rainsberger.ca',
-  integrations: [react(), svelte(), vue(), 
-    
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
-  
-  ],
+  integrations: [react(), svelte(), vue(),],
+  experimental: {
+    assets: true
+   }
 });
