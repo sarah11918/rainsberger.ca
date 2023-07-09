1156e53d97be6b58a2163652b0a72409a9b3f3b2
Sarah Rainsberger <sarah@rainsberger.ca>
2023-07-09 14:16:59 +0000

    add Expressive code!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
+import remarkExpressiveCode from 'remark-expressive-code';
+
+/** @type {import('remark-expressive-code').RemarkExpressiveCodeOptions} */
+const remarkExpressiveCodeOptions = {
+  // You can add configuration options here,
+  // see the API section for more information
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +16,12 @@ export default defineConfig({
   integrations: [react(), svelte(), vue(),],
   experimental: {
     assets: true
-   }
+   },
+   markdown: {
+    remarkPlugins: [
+      // The nested array structure below is used
+      // to pass options to the remark plugin
+      [remarkExpressiveCode, remarkExpressiveCodeOptions],
+    ],
+  }
 });
